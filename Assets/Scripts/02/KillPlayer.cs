e1cca3020a0aa8dddad7f3eafdfc9bb72ee4d40a
pupulox <kevinlopezcardenas@gmail.com>
2019-11-08 17:29:48 -0500

    killzone

---
+++ Assets/Scripts/02/KillPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    [SerializeField] Transform spawPoint;
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.transform.position = spawPoint.position;
+        }
+    }
+}
